b5fb38e1c39780e05ccb9b1f0327e8e760f03cf5
selene <selineecl25@gmail.com>
2025-10-01 21:28:08 +0300

    test

--- src/config/db.js
+++ src/config/db.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const pool = mysql.createPool({
-  host:  "127.0.0.1", // ✅ fallback to IPv4
+  host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
